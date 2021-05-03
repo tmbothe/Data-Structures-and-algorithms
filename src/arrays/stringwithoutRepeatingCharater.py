1c9a421be79e6900d5b13b8c81189f97aea47fbc
Thimothe Konchou <Thimothe.Konchou@taxact.com>
2021-05-03 08:58:17 -0500

    symmetric tree and string without repeating character

--- src/arrays/stringwithoutRepeatingCharater.py
+++ src/arrays/stringwithoutRepeatingCharater.py
@@ -1,14 +1,16 @@
 def lengthOfLongestSubstring(s):
         start =0
         maxlen=0
-        s= list(s)
-        print(s)
-        for i in range(1,len(s)):
-            current=s[start:i]
-            print(f"start={start} and i={i}")
-            print(set(current))
+        #s= list(s)
+        #print(s)
+        if len(s)<=1:
+            return len(s)
+        for i in range(0,len(s)):
+            current=s[start:i+1]
+            #print(f"start={start} and i={i}")
+            #print(set(current))
             if len(set(current))==len(current):
-                print('here')
+                #print('here')
                 maxlen=max(maxlen,len(current))
             else:
                 start+=1
